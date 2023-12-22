dbd086879f22b5ab73b9672a3b92e12060d2c765
Emmett Jaakkola <56277507+Emmennater@users.noreply.github.com>
2023-12-21 20:04:40 -0600

    Reduced Size

--- main.cpp
+++ main.cpp
@@ -10,31 +10,19 @@
 #include <bitset>
 #include <cmath>
 #include <chrono>
+#include <numeric>
 using namespace std;
 using namespace std::chrono;
 
 #define N 7
 
-void generateCombinationsHelper(vector<int>& currentCombination, int start, int n, vector<vector<int>>& result) {
-    if (start == n) {
-        result.push_back(currentCombination);
-        return;
-    }
-
-    for (int i = 0; i < n; ++i) {
-        if (find(currentCombination.begin(), currentCombination.end(), i) == currentCombination.end()) {
-            currentCombination.push_back(i);
-            generateCombinationsHelper(currentCombination, start + 1, n, result);
-            currentCombination.pop_back();
-        }
-    }
-}
-
 vector<vector<int>> generateCombinations(int n) {
     vector<vector<int>> result;
-    vector<int> currentCombination;
+    vector<int> elements(n);
+    iota(elements.begin(), elements.end(), 0);
 
-    generateCombinationsHelper(currentCombination, 0, n, result);
+    do result.push_back(elements);
+    while (next_permutation(elements.begin(), elements.end()));
 
     return result;
 }
@@ -48,23 +36,7 @@ struct Matrix {
     }
 };
 
-string hashMat(const Matrix& m) {
-    const int BYTES_STORED = (N * (N - 1) / 2) / 8 + 1;
-    char hashValue[BYTES_STORED];
-    memset(hashValue, 0, BYTES_STORED);
-
-    int idx = 0;
-    for (int i = 0; i < N - 1; ++i) {
-        for (int j = i + 1; j < N; ++j) {
-            hashValue[idx/8] |= m.edges[i][j] << (idx % 8);
-            ++idx;
-        }
-    }
-
-    return string(hashValue, BYTES_STORED);
-}
-
-string hashMatMap(const Matrix& m, const vector<int>& map) {
+string hashMat(const Matrix& m, const vector<int>& map) {
     const int BYTES_STORED = (N * (N - 1) / 2) / 8 + 1;
     char hashValue[BYTES_STORED];
     memset(hashValue, 0, BYTES_STORED);
@@ -81,7 +53,7 @@ string hashMatMap(const Matrix& m, const vector<int>& map) {
 }
 
 inline int findIsomorhpismClasses() {
-    vector<vector<int>> mappings = generateCombinations(N);
+    const vector<vector<int>> mappings = generateCombinations(N);
     unordered_map<string, bool> visited;
     vector<Matrix> matrices = vector<Matrix>{Matrix()};
     const int MAX_EDGES = N * (N - 1) / 2;
@@ -102,14 +74,14 @@ inline int findIsomorhpismClasses() {
                     m2.edges[j][i] = true;
 
                     // Rooting out visited graphs
-                    string m2hash = hashMat(m2);
+                    string m2hash = hashMat(m2, mappings[0]);
                     if (visited.find(m2hash) != visited.end()) continue;
                     
                     // Visiting all isomorphisms of unvisited graphs
                     newMatrices.push_back(m2);
-                    for (vector<int>& map : mappings) {
+                    for (const vector<int>& map : mappings) {
                         // This is the most taxing part of the algorithm (2M executions for N = 7)
-                        visited[hashMatMap(m2, map)] = true;
+                        visited[hashMat(m2, map)] = true;
                     }
                 }
             }
@@ -143,5 +115,6 @@ int main() {
     cout << "Isomorhpism Classes: " << count << endl;
     cout << duration_cast<milliseconds>(duration).count() << " milliseconds\n";
 
+    system("pause");
     return 0;
 }
